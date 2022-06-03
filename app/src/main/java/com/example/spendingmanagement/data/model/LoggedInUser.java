044fe6ed714643873ac04fa56d8530dd5af78ae0
Nguyễn Thiên Hữu <nguyenthienhuu18041999@gmail.com>
2022-06-03 13:07:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/spendingmanagement/data/model/LoggedInUser.java
@@ -0,0 +1,28 @@
+package com.example.spendingmanagement.data.model;
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+public class LoggedInUser {
+
+    private String userId;
+    private String displayName;
+    private String password;
+
+    public LoggedInUser(String userId, String displayName) {
+        this.userId = userId;
+        this.displayName = displayName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
